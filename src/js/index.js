4eb13bc9673b3c9afb9695ed2a100c8984eff09c
Ryan Castañeda <castaneda.ry@gmail.com>
2024-02-23 17:09:33 +0800

    Added modal functionality

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,29 @@ import '../css/global.css';
 import '../scss/global.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const main = document.querySelector('#main');
-  const counter = main.querySelector('#counter');
+  const modalTrigger = document.querySelectorAll('.modal-trigger');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('imageModal');
+  const modalImage = document.getElementById('modalImage');
 
-  main.addEventListener('click', (event) => {
-    const { id, type } = event.target;
-
-    if (type === 'button') {
-      const currentCount = Number(counter.textContent);
-
-      switch (id) {
-        case 'increment': {
-          counter.textContent = currentCount + 1;
-          break;
-        }
-        case 'decrement': {
-          counter.textContent = currentCount - 1;
-          break;
-        }
-      }
-    }
+  modalTrigger.forEach(function (trigger) {
+    trigger.addEventListener('click', function () {
+      const imageUrl = this.previousSibling.getAttribute('srcset');
+      modalImage.src = imageUrl;
+      modal.style.display = 'block';
+    });
   });
+
+  close.addEventListener('click', closeModal)
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  };
 });
+
+function closeModal() {
+  const modal = document.getElementById('imageModal');
+  modal.style.display = 'none';
+}
